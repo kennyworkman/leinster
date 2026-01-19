d2a57e075835bd01a7f62d1cde5cbebbabe84ae1
Kenny Workman <kennyworkman@sbcglobal.net>
2026-01-18 17:41:24 -0800

    Add table of contents to combined notes

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+cat > toc.md << 'EOF'
+# Basic Category Theory - Notes
+
+## Contents
+
+1. Free categories and graphs
+2. Representables
+3. Yoneda lemma
+4. Embedding and applications
+5. Limits
+6. Adjoints
+EOF
+
+pandoc toc.md -o toc.pdf --pdf-engine=pdflatex -V geometry:margin=1in
+
 pdfunite \
+  toc.pdf \
   3/3_2.pdf \
   4/4_1.pdf \
   4/4_2.pdf \
@@ -8,3 +24,5 @@ pdfunite \
   5/5.pdf \
   6/6.pdf \
   notes.pdf
+
+rm toc.md toc.pdf
